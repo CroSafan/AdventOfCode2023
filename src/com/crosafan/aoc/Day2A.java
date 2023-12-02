4b7d34b2fb4a3573405c02dba0ab93edff44190c
antuns <crosfan@gmail.com>
2023-12-02 12:26:50 +0100

    Day 2 part 1 solved

---
+++ src/com/crosafan/aoc/Day2A.java
@@ -0,0 +1,47 @@
+package com.crosafan.aoc;
+
+import java.util.List;
+
+public class Day2A {
+
+	public int solve(List<String> inputLines, int redCountInput, int greenCountInput, int blueCountInput) {
+		// testLines.add("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+		int sum = 0;
+		for (String line : inputLines) {
+			int gameId = Integer.parseInt(line.split(":")[0].split(" ")[1]);
+			String cubes = line.split(":")[1].trim();
+			String[] cubeSubsets = cubes.split(";");
+			boolean isPossible = true;
+			for (String subset : cubeSubsets) {
+				int redCount = 0, greenCount = 0, blueCount = 0;
+
+				String[] grab = subset.split(",");
+				for (String cube : grab) {
+					int count = Integer.parseInt(cube.trim().split(" ")[0]);
+					String color = cube.trim().split(" ")[1];
+					if (color.equals("red")) {
+						redCount += count;
+					} else if (color.equals("green")) {
+						greenCount += count;
+					} else if (color.equals("blue")) {
+						blueCount += count;
+
+					}
+				}
+
+				if (redCount > redCountInput || greenCount > greenCountInput || blueCount > blueCountInput) {
+					isPossible = false;
+				}
+
+			}
+
+			if (isPossible) {
+				sum += gameId;
+			}
+
+		}
+
+		return sum;
+	}
+
+}
--- src/com/crosafan/aoc/Main.java
+++ src/com/crosafan/aoc/Main.java
@@ -12,24 +12,24 @@ public class Main {
 
 		List<String> allLines = null;
 		try {
-			allLines = Files.readAllLines(Paths.get("resources/input01.txt"));
+			allLines = Files.readAllLines(Paths.get("resources/input02.txt"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		Day1B day1B = new Day1B();
+		Day2A day2A = new Day2A();
 
 		ArrayList<String> testLines = new ArrayList<String>();
 
-		testLines.add("two1nine");
-		testLines.add("eightwothree");
-		testLines.add("abcone2threexyz");
-		testLines.add("xtwone3four");
-		testLines.add("4nineeightseven2");
-		testLines.add("zoneight234");
-		testLines.add("7pqrstsixteen");
+		testLines.add("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+		testLines.add("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue");
+		testLines.add("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red");
+		testLines.add("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red");
+		testLines.add("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green");
 
-		int result = day1B.solve(allLines);
+		int redCount = 12, greenCount = 13, blueCount = 14;
+		
+		int result = day2A.solve(allLines,redCount,greenCount,blueCount);
 
 		System.out.println(result);
 

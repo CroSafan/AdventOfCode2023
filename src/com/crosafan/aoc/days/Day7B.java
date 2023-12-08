d1072798c3ff06db1b21fb5b7c87b345a2ed1362
antuns <crosfan@gmail.com>
2023-12-08 20:50:33 +0100

    Day 7 part 2 solved

--- src/com/crosafan/aoc/days/Day7B.java
+++ src/com/crosafan/aoc/days/Day7B.java
@@ -48,7 +48,7 @@ public class Day7B {
 		};
 		HashMap<Integer, ArrayList<Hand>> handMap = new HashMap<Integer, ArrayList<Hand>>();
 		Collections.sort(hands, handScoreComparator);
-		//grouping scores
+		// grouping scores
 		for (Hand hand : hands) {
 
 			if (handMap.containsKey(hand.getScore())) {
@@ -83,9 +83,29 @@ public class Day7B {
 		public Hand(String cards, int bid) {
 			this.cards = cards;
 			this.bid = bid;
-			this.score = scoreHand(cards);
+			this.score = maxScoreHand(cards);
 		}
 
+		private int maxScoreHand(String hand) {
+			int maxScore = 0;
+
+			if (hand.contains("J")) {
+				String handCopy = hand;
+				String[] possibilities = new String[] { "A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2" };
+				for (String possibility : possibilities) {
+					if (scoreHand(handCopy.replace("J", possibility)) > maxScore) {
+						maxScore = scoreHand(handCopy.replace("J", possibility));
+					}
+
+				}
+
+			} else {
+				return scoreHand(hand);
+			}
+
+			return maxScore;
+
+		}
 
 		private int scoreHand(String hand) {
 			int score = 0;
@@ -255,34 +275,34 @@ public class Day7B {
 			case 'Q': {
 				return 11;
 			}
-			case 'J': {
+			case 'T': {
 				return 10;
 			}
-			case 'T': {
+			case '9': {
 				return 9;
 			}
-			case '9': {
+			case '8': {
 				return 8;
 			}
-			case '8': {
+			case '7': {
 				return 7;
 			}
-			case '7': {
+			case '6': {
 				return 6;
 			}
-			case '6': {
+			case '5': {
 				return 5;
 			}
-			case '5': {
+			case '4': {
 				return 4;
 			}
-			case '4': {
+			case '3': {
 				return 3;
 			}
-			case '3': {
+			case '2': {
 				return 2;
 			}
-			case '2': {
+			case 'J': {
 				return 1;
 			}
 			default:

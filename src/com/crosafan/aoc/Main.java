9d6831b241ae2272806415e27cffe897e47035af
antuns <crosfan@gmail.com>
2023-12-08 20:23:57 +0100

    Day 7 part 1 solved

--- src/com/crosafan/aoc/Main.java
+++ src/com/crosafan/aoc/Main.java
@@ -6,11 +6,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.crosafan.aoc.days.Day4B;
-import com.crosafan.aoc.days.Day5A;
-import com.crosafan.aoc.days.Day5B;
-import com.crosafan.aoc.days.Day6A;
-import com.crosafan.aoc.days.Day6B;
+import com.crosafan.aoc.days.Day7B;
 
 public class Main {
 
@@ -18,18 +14,21 @@ public class Main {
 
 		List<String> allLines = null;
 		try {
-			allLines = Files.readAllLines(Paths.get("resources/input06.txt"));
+			allLines = Files.readAllLines(Paths.get("resources/input07.txt"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		Day6B day6B = new Day6B();
+		Day7B day7B = new Day7B();
 
 		ArrayList<String> testLines = new ArrayList<String>();
 
-		testLines.add("Time:      7  15   30");
-		testLines.add("Distance:  9  40  200");
-		int result = day6B.solve(allLines);
+		testLines.add("32T3K 765");
+		testLines.add("T55J5 684");
+		testLines.add("KK677 28");
+		testLines.add("KTJJT 220");
+		testLines.add("QQQJA 483");
+		int result = day7B.solve(allLines);
 
 		System.out.println("Result:" + result);
 
---
+++ src/com/crosafan/aoc/days/Day7A.java
@@ -0,0 +1,326 @@
+package com.crosafan.aoc.days;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Day7A {
+
+	public int solve(List<String> allLines) {
+		int result = 0;
+		ArrayList<Hand> hands = new ArrayList<Day7A.Hand>();
+
+		for (String line : allLines) {
+			Hand hand = new Hand(line.split(" ")[0], Integer.parseInt(line.split(" ")[1]));
+			hands.add(hand);
+		}
+
+		Comparator<Hand> handScoreComparator = new Comparator<Hand>() {
+
+			@Override
+			public int compare(Hand h1, Hand h2) {
+				return Integer.compare(h1.getScore(), h2.getScore());
+			}
+		};
+
+		Comparator<Hand> weightComparator = new Comparator<Hand>() {
+
+			@Override
+			public int compare(Hand h1, Hand h2) {
+
+				for (int j = 0; j < 5; j++) {
+
+					if (h1.getCards().charAt(j) != h2.getCards().charAt(j)) {
+						return Integer.compare(Hand.scoreCard(h1.getCards().charAt(j)),
+								Hand.scoreCard(h2.getCards().charAt(j)));
+					}
+
+				}
+
+				return 0;
+			}
+		};
+		HashMap<Integer, ArrayList<Hand>> handMap = new HashMap<Integer, ArrayList<Hand>>();
+		Collections.sort(hands, handScoreComparator);
+		//grouping scores
+		for (Hand hand : hands) {
+
+			if (handMap.containsKey(hand.getScore())) {
+				handMap.get(hand.getScore()).add(hand);
+			} else {
+				ArrayList<Day7A.Hand> subHands = new ArrayList<Day7A.Hand>();
+				subHands.add(hand);
+				handMap.put(hand.getScore(), subHands);
+			}
+
+		}
+
+		hands.clear();
+		for (int key : handMap.keySet()) {
+			Collections.sort(handMap.get(key), weightComparator);
+			hands.addAll(handMap.get(key));
+		}
+
+		for (int i = 0; i < hands.size(); i++) {
+			result += (i + 1) * hands.get(i).getBid();
+		}
+
+		return result;
+
+	}
+
+	public class Hand {
+		private String cards;
+		private int bid;
+		private int score;
+
+		public Hand(String cards, int bid) {
+			this.cards = cards;
+			this.bid = bid;
+			this.score = scoreHand(cards);
+		}
+
+
+		private int scoreHand(String hand) {
+			int score = 0;
+
+			if (isFiveOfKind(hand)) {
+				score = 7;
+			} else if (isFourOfKind(hand)) {
+				score = 6;
+			} else if (isFullHouse(hand)) {
+				score = 5;
+			} else if (isThreeOfKind(hand)) {
+				score = 4;
+			} else if (isTwoPair(hand)) {
+				score = 3;
+			} else if (isOnePair(hand)) {
+				score = 2;
+			} else if (isHighCard(hand)) {
+				score = 1;
+			}
+
+			return score;
+		}
+
+		private boolean isHighCard(String hand) {
+			return hand.chars().mapToObj(e -> Character.toString((char) e)).distinct().toList().size() == 5;
+		}
+
+		private boolean isOnePair(String hand) {
+			Map<String, Long> output = Arrays.stream(hand.split(""))
+					.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+			int pairCount = 0;
+			for (String key : output.keySet()) {
+				if (output.get(key) == 2L) {
+					pairCount++;
+				}
+			}
+			if (pairCount == 1) {
+				return true;
+			}
+
+			return false;
+		}
+
+		private boolean isTwoPair(String hand) {
+			Map<String, Long> output = Arrays.stream(hand.split(""))
+					.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+			int pairCount = 0;
+			for (String key : output.keySet()) {
+				if (output.get(key) == 2L) {
+					pairCount++;
+				}
+			}
+			if (pairCount == 2) {
+				return true;
+			}
+
+			return false;
+		}
+
+		public boolean isThreeOfKind(String hand) {
+			Map<String, Long> output = Arrays.stream(hand.split(""))
+					.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+			if (output.keySet().size() == 3) {
+				boolean threeDisinct = false;
+				boolean twoDistinct = false;
+				for (String key : output.keySet()) {
+					if (output.get(key) == 2L) {
+						twoDistinct = true;
+					}
+					if (output.get(key) == 3L) {
+						threeDisinct = true;
+					}
+					if (threeDisinct && !twoDistinct) {
+						return true;
+					}
+
+				}
+			}
+
+			return false;
+		}
+
+		// Full house, where three cards have the same label, and the remaining two
+		// cards share a different label: 23332
+		public boolean isFullHouse(String hand) {
+			Map<String, Long> output = Arrays.stream(hand.split(""))
+					.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+			if (output.keySet().size() == 2) {
+				boolean threeDisinct = false;
+				boolean twoDistinct = false;
+				for (String key : output.keySet()) {
+					if (output.get(key) == 2L) {
+						twoDistinct = true;
+					}
+					if (output.get(key) == 3L) {
+						threeDisinct = true;
+					}
+					if (threeDisinct && twoDistinct) {
+						return true;
+					}
+
+				}
+			}
+
+			return false;
+		}
+
+		public boolean isFourOfKind(String hand) {
+
+			Map<String, Long> output = Arrays.stream(hand.split(""))
+					.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+			for (String key : output.keySet()) {
+				if (output.get(key) == 4L) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public boolean isFiveOfKind(String hand) {
+			if (hand.chars().filter(ch -> ch == hand.charAt(0)).count() == 5) {
+				return true;
+			}
+
+			return false;
+		}
+
+		public String getCards() {
+			return cards;
+		}
+
+		public void setCards(String cards) {
+			this.cards = cards;
+		}
+
+		public int getBid() {
+			return bid;
+		}
+
+		public void setBid(int bid) {
+			this.bid = bid;
+		}
+
+		public int getScore() {
+			return score;
+		}
+
+		public void setScore(int score) {
+			this.score = score;
+		}
+
+		public static int scoreCard(char card) {
+			int score = 0;
+			switch (card) {
+			case 'A': {
+				return 13;
+			}
+			case 'K': {
+				return 12;
+			}
+			case 'Q': {
+				return 11;
+			}
+			case 'J': {
+				return 10;
+			}
+			case 'T': {
+				return 9;
+			}
+			case '9': {
+				return 8;
+			}
+			case '8': {
+				return 7;
+			}
+			case '7': {
+				return 6;
+			}
+			case '6': {
+				return 5;
+			}
+			case '5': {
+				return 4;
+			}
+			case '4': {
+				return 3;
+			}
+			case '3': {
+				return 2;
+			}
+			case '2': {
+				return 1;
+			}
+			default:
+				break;
+			}
+
+			return score;
+
+		}
+
+	}
+
+	public void test() {
+		Hand testHand = new Hand("AAAAA", 0);
+
+		if (testHand.isFiveOfKind("AAAAA")) {
+			System.out.println("It is five kind");
+		}
+
+		if (testHand.isFourOfKind("A3AAA")) {
+			System.out.println("It is four kind");
+		}
+		if (testHand.isFullHouse("23332")) {
+			System.out.println("Full house");
+		}
+		if (testHand.isThreeOfKind("TTT98")) {
+			System.out.println("Three of a kind");
+		}
+		if (testHand.isTwoPair("23432")) {
+			System.out.println("Two pair");
+		}
+
+		if (testHand.isOnePair("A23A4")) {
+			System.out.println("One pair");
+		}
+		if (testHand.isHighCard("23456")) {
+			System.out.println("High card");
+		}
+
+	}
+}

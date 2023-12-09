62048138a79a7edeb7553e4ff5cbfc05a6c0cdeb
antuns <crosfan@gmail.com>
2023-12-09 18:46:11 +0100

    Day 9 Part 2 finished

--- src/com/crosafan/aoc/Main.java
+++ src/com/crosafan/aoc/Main.java
@@ -6,7 +6,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.crosafan.aoc.days.Day9A;
+import com.crosafan.aoc.days.Day9B;
 
 public class Main {
 
@@ -19,7 +19,7 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		Day9A day9A = new Day9A();
+		Day9B day9B = new Day9B();
 
 		ArrayList<String> testLines = new ArrayList<String>();
 		
@@ -27,7 +27,7 @@ public class Main {
 		testLines.add("1 3 6 10 15 21");
 		testLines.add("10 13 16 21 30 45");
 
-		long result = day9A.solve(allLines);
+		long result = day9B.solve(allLines);
 
 		System.out.println("Result:" + result);
 
---
+++ src/com/crosafan/aoc/days/Day9B.java
@@ -0,0 +1,67 @@
+package com.crosafan.aoc.days;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Day9B {
+
+	public int solve(List<String> allLines) {
+		int result = 0;
+
+		for (String line : allLines) {
+
+			result += predictValue(line);
+		}
+
+		return result;
+
+	}
+
+	private int predictValue(String line) {
+		ArrayList<ArrayList<Integer>> rows = new ArrayList<ArrayList<Integer>>();
+
+		String[] values = line.split(" ");
+
+		rows.add(new ArrayList<Integer>());
+
+		for (String value : values) {
+			rows.get(0).add(Integer.valueOf(value));
+		}
+
+		for (int i = 0; i < rows.size(); i++) {
+			rows.add(new ArrayList<Integer>());
+			for (int j = 0; j < rows.get(i).size() - 1; j++) {
+				int diff =rows.get(i).get(j + 1)-rows.get(i).get(j);
+				rows.get(i + 1).add(diff);
+			}
+			if (rows.get(i + 1).stream().distinct().toList().get(0) == 0 && rows.get(i + 1).isEmpty()
+					|| rows.get(i + 1).stream().allMatch(rows.get(i + 1).get(0)::equals)) {
+				break;
+			}
+		}
+
+//		for(ArrayList<Integer> row : rows) {
+//			row.sort(Comparator.reverseOrder());
+//		}
+
+		int result = 0;
+		for (int i = rows.size() - 1; i >= 0; i--) {
+			int firstNum = rows.get(i).get(0);
+
+			result = firstNum - result;
+
+		}
+
+		System.out.println(result);
+
+		return result;
+	}
+
+}

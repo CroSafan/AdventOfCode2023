c7b88d4e86258b85dfd80b43ac012c4e92c90321
antuns <crosfan@gmail.com>
2023-12-09 17:42:34 +0100

    Day 9 Part 1 finished

--- src/com/crosafan/aoc/Main.java
+++ src/com/crosafan/aoc/Main.java
@@ -6,7 +6,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.crosafan.aoc.days.Day8B;
+import com.crosafan.aoc.days.Day9A;
 
 public class Main {
 
@@ -14,27 +14,20 @@ public class Main {
 
 		List<String> allLines = null;
 		try {
-			allLines = Files.readAllLines(Paths.get("resources/input08.txt"));
+			allLines = Files.readAllLines(Paths.get("resources/input09.txt"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		Day8B day8B = new Day8B();
+		Day9A day9A = new Day9A();
 
 		ArrayList<String> testLines = new ArrayList<String>();
-
-		testLines.add("LR");
-		testLines.add("");
-		testLines.add("11A = (11B, XXX)");
-		testLines.add("11B = (XXX, 11Z)");
-		testLines.add("11Z = (11B, XXX)");
-		testLines.add("22A = (22B, XXX)");
-		testLines.add("22B = (22C, 22C)");
-		testLines.add("22C = (22Z, 22Z)");
-		testLines.add("22Z = (22B, 22B)");
-		testLines.add("XXX = (XXX, XXX)");
 		
-		long result = day8B.solve(allLines);
+		testLines.add("0 3 6 9 12 15");
+		testLines.add("1 3 6 10 15 21");
+		testLines.add("10 13 16 21 30 45");
+
+		long result = day9A.solve(allLines);
 
 		System.out.println("Result:" + result);
 
---
+++ src/com/crosafan/aoc/days/Day9A.java
@@ -0,0 +1,62 @@
+package com.crosafan.aoc.days;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Day9A {
+
+	public int solve(List<String> allLines) {
+		int result = 0;
+
+		for (String line : allLines) {
+
+			result += predictValue(line);
+		}
+
+		return result;
+
+	}
+
+	private int predictValue(String line) {
+		ArrayList<ArrayList<Integer>> rows = new ArrayList<ArrayList<Integer>>();
+
+		String[] values = line.split(" ");
+
+		rows.add(new ArrayList<Integer>());
+
+		for (String value : values) {
+			rows.get(0).add(Integer.valueOf(value));
+		}
+
+		for (int i = 0; i < rows.size(); i++) {
+			rows.add(new ArrayList<Integer>());
+			for (int j = 0; j < rows.get(i).size() - 1; j++) {
+				int diff =rows.get(i).get(j + 1)-rows.get(i).get(j);
+				rows.get(i + 1).add(diff);
+			}
+			if (rows.get(i + 1).stream().distinct().toList().get(0) == 0 && rows.get(i + 1).isEmpty() || rows.get(i + 1).stream().allMatch(rows.get(i + 1).get(0)::equals)) {
+				break;
+			}
+		}
+		int result=0;
+		for(int i = rows.size()-1;i>=0;i--) {
+			
+			result=rows.get(i).get(rows.get(i).size()-1)+result;
+			
+		}
+		
+		
+		
+//		System.out.println(result);
+
+		return result;
+	}
+
+}
